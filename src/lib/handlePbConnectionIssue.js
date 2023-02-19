a533861547391e61e37c1f8edaa546d341027099
babakfp <44144724+babakfp@users.noreply.github.com>
2023-02-19 15:30:14 -0800

    Changed error message

--- src/lib/handlePbConnectionIssue.js
+++ src/lib/handlePbConnectionIssue.js
@@ -2,6 +2,9 @@ import { error } from "@sveltejs/kit"
 
 export function handlePbConnectionIssue(status) {
 	if (status === 0) {
-		throw error(500, "Unable to communicate with the database.")
+		throw error(
+			500,
+			"Sorry, we were unable to communicate with the database."
+		)
 	}
 }

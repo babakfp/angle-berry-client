43ba298bcde1a1319f643871333da6aac511b558
babakfp <44144724+babakfp@users.noreply.github.com>
2023-03-13 18:56:25 -0700

    Changes time style

--- src/lib/formatTimeByAMPM.js
+++ src/lib/formatTimeByAMPM.js
@@ -1,9 +1,9 @@
 export function formatTimeByAMPM(date) {
 	let hours = date.getHours()
 	let minutes = date.getMinutes()
-	const ampm = hours >= 12 ? "pm" : "am"
+	const ampm = hours >= 12 ? "PM" : "AM"
 	hours = hours % 12
 	hours = hours ? hours : 12 // The hour '0' should be '12'
 	minutes = minutes < 10 ? `0${minutes}` : minutes
-	return `${hours}:${minutes}${ampm}`
+	return `${hours}:${minutes} ${ampm}`
 }

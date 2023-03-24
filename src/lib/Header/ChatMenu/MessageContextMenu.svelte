b0612926e43d43f4f7eefdb0d82948e303d3081b
babakfp <44144724+babakfp@users.noreply.github.com>
2023-03-24 01:55:44 -0700

    Stuff

--- src/lib/Header/ChatMenu/MessageContextMenu.svelte
+++ src/lib/Header/ChatMenu/MessageContextMenu.svelte
@@ -21,10 +21,11 @@
 			}}
 		>
 			<!-- prettier-ignore -->
-			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
 				<path stroke-linecap="round" stroke-linejoin="round" d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3" />
 			</svg>
-			Reply
+			<!-- Remove the styles if you change the font -->
+			<span class="pt-0.5">Reply</span>
 		</button>
 	</li>
 </ul>

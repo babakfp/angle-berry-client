ed117de8f594c59c06e40490697367f2bc7955e2
babakfp <44144724+babakfp@users.noreply.github.com>
2023-03-24 01:03:47 -0700

    Fixed reply styles

--- src/lib/Header/ChatMenu/Reply.svelte
+++ src/lib/Header/ChatMenu/Reply.svelte
@@ -32,10 +32,10 @@
 </script>
 
 <div
-	class="flex items-stretch justify-between border-t border-white/5 text-sm shadow-[0_-1px_3px_0_rgb(0_0_0_/_0.1),_0_-1px_2px_-1px_rgb(0_0_0_/_0.1)]"
+	class="grid grid-cols-[1fr_auto] items-stretch justify-between border-t border-white/5 text-sm shadow-[0_-1px_3px_0_rgb(0_0_0_/_0.1),_0_-1px_2px_-1px_rgb(0_0_0_/_0.1)]"
 >
 	<button
-		class="block w-full py-4 pl-4 text-left line-clamp-2 outline-inset"
+		class="block w-full py-4 pl-4 text-left line-clamp-2 outline-inset hover:bg-white/5"
 		href="#{$messageThatWeAreReplyingTo.id}"
 		type="button"
 		on:click={handleClick}
@@ -46,13 +46,13 @@
 		<div>{$messageThatWeAreReplyingTo.content}</div>
 	</button>
 	<button
-		class="flex h-8 items-center px-4 outline-inset hover:bg-gray-700"
+		class="flex items-center px-4 outline-inset hover:bg-white/5"
 		type="button"
 		on:click={() => isReplying.set(false)}
 	>
 		<!-- prettier-ignore -->
 		<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-						<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-					</svg>
+			<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+		</svg>
 	</button>
 </div>

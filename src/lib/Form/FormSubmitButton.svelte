864cc504e53bd43444840ee7d92faccbcfe7e20e
babakfp <44144724+babakfp@users.noreply.github.com>
2023-03-23 23:40:23 -0700

    Removed outline-0 and outline-4

--- src/lib/Form/FormSubmitButton.svelte
+++ src/lib/Form/FormSubmitButton.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <button
-	class="{className} block h-14 w-full rounded bg-white text-gray-800 outline-4"
+	class="{className} block h-14 w-full rounded bg-white text-gray-800"
 	{disabled}
 >
 	<slot />

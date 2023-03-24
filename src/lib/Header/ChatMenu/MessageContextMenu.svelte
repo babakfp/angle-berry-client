a0befffea89a1f4a0def442435c29e9939bdf6e9
babakfp <44144724+babakfp@users.noreply.github.com>
2023-03-24 00:16:15 -0700

    Stuff

--- src/lib/Header/ChatMenu/MessageContextMenu.svelte
+++ src/lib/Header/ChatMenu/MessageContextMenu.svelte
@@ -6,8 +6,8 @@
 </script>
 
 <ul
-	class="absolute top-full max-h-56 w-32 -translate-y-3 overflow-y-auto rounded bg-gray-700 text-xs shadow hide
-		{user.id === message.expand.user.id && 'right-0'}
+	class="absolute top-full max-h-56 w-32 -translate-y-5 overflow-y-auto rounded bg-gray-700 text-xs shadow hide
+		{user.id === message.expand.user.id ? 'right-4' : 'left-4'}
 		{isContextMenuOpen && 'show'}
 		"
 >

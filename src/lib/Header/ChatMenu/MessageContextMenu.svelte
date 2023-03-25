fa50f828b100e99b8ac866bb1a91df4bf796c5b9
babakfp <44144724+babakfp@users.noreply.github.com>
2023-03-25 09:15:47 -0700

    Using `if` block instead of hiding it with css

--- src/lib/Header/ChatMenu/MessageContextMenu.svelte
+++ src/lib/Header/ChatMenu/MessageContextMenu.svelte
@@ -6,27 +6,28 @@
 	export let isContextMenuOpen = false
 </script>
 
-<ul
-	class="MessageContextMenu absolute top-full max-h-56 w-36 -translate-y-5 overflow-y-auto rounded bg-gray-700 text-xs shadow hide
+{#if isContextMenuOpen}
+	<ul
+		class="MessageContextMenu absolute top-full max-h-56 w-36 -translate-y-5 overflow-y-auto rounded bg-gray-700 text-xs shadow
 		{user.id === message.expand.user.id ? 'right-4' : 'left-4'}
-		{isContextMenuOpen && 'show'}
 		"
->
-	<li class="flex">
-		<button
-			class="flex w-full items-center gap-2 rounded px-4 py-2 hover:text-white"
-			on:click={() => {
-				isContextMenuOpen = false
-				isReplying.set(true)
-				messageThatWeAreReplyingTo.set(message)
-			}}
-		>
-			<!-- prettier-ignore -->
-			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+	>
+		<li class="flex">
+			<button
+				class="flex w-full items-center gap-2 rounded px-4 py-2 hover:text-white"
+				on:click={() => {
+					isContextMenuOpen = false
+					isReplying.set(true)
+					messageThatWeAreReplyingTo.set(message)
+				}}
+			>
+				<!-- prettier-ignore -->
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
 				<path stroke-linecap="round" stroke-linejoin="round" d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3" />
 			</svg>
-			<!-- Remove the styles if you change the font -->
-			<span class="pt-0.5">Reply</span>
-		</button>
-	</li>
-</ul>
+				<!-- Remove the styles if you change the font -->
+				<span class="pt-0.5">Reply</span>
+			</button>
+		</li>
+	</ul>
+{/if}

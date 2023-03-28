545d36d05196d00fc27554a2cb393e2b886de3ce
babakfp <44144724+babakfp@users.noreply.github.com>
2023-03-28 16:19:01 -0700

    Cleaned code

--- src/lib/Header/ChatMenu/Message/MessageReplyPreview.svelte
+++ src/lib/Header/ChatMenu/Message/MessageReplyPreview.svelte
@@ -4,22 +4,21 @@
 	// Message that we are replying to.
 	export let replyTargetMessage
 
-	let intervalId
-	let timeoutId
+	// IDs to keep a track of things.
+	let intervalId, timeoutId
+
+	// When clicked, starts scrolling to the message being replied to.
+	function handleGoToMessage() {
+		const result = goToMessage(replyTargetMessage.id, intervalId, timeoutId)
+		intervalId = result?.intervalId
+		timeoutId = result?.timeoutId
+	}
 </script>
 
 {#if replyTargetMessage?.content}
 	<button
 		class="mb-2 -ml-3 block border-l-2 pl-2 text-left text-xs text-white/80 hover:text-white/100"
-		on:click={() => {
-			const result = goToMessage(
-				replyTargetMessage.id,
-				intervalId,
-				timeoutId
-			)
-			intervalId = result?.intervalId
-			timeoutId = result?.timeoutId
-		}}
+		on:click={handleGoToMessage}
 	>
 		<div class="flex gap-1">
 			<span class="font-semibold">

755b7c3b37e6677cd645808c0cab5bd6d1bddec6
babakfp <44144724+babakfp@users.noreply.github.com>
2023-03-28 09:20:30 -0700

    Stuff

--- src/lib/Modal.svelte
+++ src/lib/Modal.svelte
@@ -1,7 +1,5 @@
 <script>
-	// import OutClick from "svelte-outclick"
 	export let isOpen = false
-	// export let toggleElements = false
 </script>
 
 <svelte:window
@@ -11,20 +9,14 @@
 />
 
 {#if isOpen}
-	<!-- <OutClick
-		on:outclick={() => (isOpen = false)}
-		excludeElements={toggleElements}
-	> -->
 	<div
 		class="fixed inset-0 z-50 flex h-screen w-screen items-center justify-center bg-gray-800/50 p-8"
 		on:click={() => (isOpen = false)}
 	>
-		<!-- Inner -->
 		<div
 			class="max-w-md overflow-y-auto rounded bg-gray-700 p-6 drop-shadow"
 		>
 			<slot />
 		</div>
 	</div>
-	<!-- </OutClick> -->
 {/if}

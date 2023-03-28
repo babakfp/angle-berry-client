d3d01225bbf328f9e403d77b1c6c5534d9655ed8
babakfp <44144724+babakfp@users.noreply.github.com>
2023-03-28 16:02:20 -0700

    Clean code

--- src/lib/Header/ChatMenu/Message/dateToAMPM.js
+++ src/lib/Header/ChatMenu/Message/dateToAMPM.js
@@ -8,7 +8,7 @@ export function dateToAMPM(date) {
 	let minutes = date.getMinutes()
 	const ampm = hours >= 12 ? "PM" : "AM"
 	hours = hours % 12
-	hours = hours ? hours : 12 // The hour '0' should be '12'
+	hours = hours || 12 // The hour '0' should be '12'
 	minutes = minutes < 10 ? `0${minutes}` : minutes
 	return `${hours}:${minutes} ${ampm}`
 }

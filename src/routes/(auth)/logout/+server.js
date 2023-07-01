5a2c8cfc2c3c6e3aa7e78de0bf6e752af5c008d7
babakfp <babak.bxf@gmail.com>
2023-07-01 16:02:49 -0700

    Stuff

--- src/routes/(auth)/logout/+server.js
+++ src/routes/(auth)/logout/+server.js
@@ -1,5 +1,9 @@
 import { redirect } from "@sveltejs/kit"
 
+export function GET({ locals }) {
+    if (locals.user) throw redirect(303, "/")
+}
+
 export function POST({ locals }) {
     locals.pb.authStore.clear()
     locals.user = null

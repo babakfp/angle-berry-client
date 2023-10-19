42b77d4a99abcbf30ce80ba5364265311f4929c0
babakfp <babak.bxf@gmail.com>
2023-10-18 17:11:39 -0700

    Changed icons style

--- src/lib/icons/IconTrash.svelte
+++ src/lib/icons/IconTrash.svelte
@@ -4,4 +4,4 @@
 </script>
 
 <!-- prettier-ignore -->
-<svg class={_class} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><path d="M216 48H40a8 8 0 0 0 0 16h8v144a16 16 0 0 0 16 16h128a16 16 0 0 0 16-16V64h8a8 8 0 0 0 0-16Zm-24 160H64V64h128ZM80 24a8 8 0 0 1 8-8h80a8 8 0 0 1 0 16H88a8 8 0 0 1-8-8Z"/></svg>
+<svg class="icon {_class}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><path d="M216 48H40a8 8 0 0 0 0 16h8v144a16 16 0 0 0 16 16h128a16 16 0 0 0 16-16V64h8a8 8 0 0 0 0-16Zm-24 160H64V64h128ZM80 24a8 8 0 0 1 8-8h80a8 8 0 0 1 0 16H88a8 8 0 0 1-8-8Z"/></svg>

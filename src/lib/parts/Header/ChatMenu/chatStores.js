b99a2c3dac10ee9493c1cfc3176e0051eb89253f
babakfp <babak.bxf@gmail.com>
2023-10-20 06:11:09 -0700

    Formatted

--- src/lib/parts/Header/ChatMenu/chatStores.js
+++ src/lib/parts/Header/ChatMenu/chatStores.js
@@ -3,11 +3,8 @@ import { writable } from "svelte/store"
 export const isContextMenuOpen = writable(false)
 export const contextMenuTargetEvent = writable(null)
 export const contextMenuTargetMessage = writable(null)
-
 export const messageIdsToDelete = writable([])
-
 export const messageIdToEdit = writable(null)
-
 export const isReplying = writable(false)
 export const replyTargetMessage = writable({})
 export const messageInputElement = writable(undefined)

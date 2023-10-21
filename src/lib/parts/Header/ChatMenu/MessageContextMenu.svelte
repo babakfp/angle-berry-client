0531dc196ab52c5716cd62b86ac54d3da500ce6c
babakfp <babak.bxf@gmail.com>
2023-10-20 19:09:42 -0700

    Fixed chat message context menu position

--- src/lib/parts/Header/ChatMenu/MessageContextMenu.svelte
+++ src/lib/parts/Header/ChatMenu/MessageContextMenu.svelte
@@ -3,39 +3,38 @@
     export let isOpen = false
 
     /** @type {HTMLDivElement} */
-    let contextMenu
+    let contextMenu = null
 
     $: if (contextMenu && isOpen) {
-        let positionX = e.clientX
-        let positionY = e.clientY
+        let x = e.clientX
+        let y = e.clientY
 
         if (e.clientX + contextMenu.offsetWidth > window.innerWidth) {
-            positionX = positionX - contextMenu.offsetWidth
+            x = x - contextMenu.offsetWidth
         }
         if (e.clientY + contextMenu.offsetHeight > window.innerHeight) {
-            positionY = positionY - contextMenu.offsetHeight
+            y = y - contextMenu.offsetHeight
             contextMenu.style.transform = "translateY(1.25rem)"
         }
 
-        contextMenu.style.left = `${positionX}px`
-        contextMenu.style.top = `${positionY}px`
+        contextMenu.style.left = `${x}px`
+        contextMenu.style.top = `${y}px`
     }
 </script>
 
-<div
-    bind:this={contextMenu}
-    class="absolute z-50 -translate-y-5 {!isOpen && 'hidden'}"
->
-    <!-- Close the menu when moved out of the bunding area -->
-    <div
-        class="absolute -inset-8 -z-1"
-        on:pointerdown={() => (isOpen = false)}
-        on:contextmenu|preventDefault
-        on:pointerleave={() => (isOpen = false)}
-    />
-    <ul
-        class="max-h-56 min-w-36 max-w-48 overflow-y-auto overscroll-y-contain rounded bg-gray-600 p-1 text-xs shadow"
-    >
-        <slot />
-    </ul>
-</div>
+{#if isOpen}
+    <div bind:this={contextMenu} class="absolute z-50 -translate-y-5">
+        <!-- Close the menu when moved out of the bunding area -->
+        <div
+            class="absolute -inset-8 -z-1"
+            on:pointerdown={() => (isOpen = false)}
+            on:contextmenu|preventDefault
+            on:pointerleave={() => (isOpen = false)}
+        />
+        <ul
+            class="max-h-56 min-w-36 max-w-48 overflow-y-auto overscroll-y-contain rounded bg-gray-600 p-1 text-xs shadow"
+        >
+            <slot />
+        </ul>
+    </div>
+{/if}

7e174d0ced4850e919f40c4f895b0388b02374b3
babakfp <babak.bxf@gmail.com>
2023-10-28 16:36:40 -0700

    Clean up

--- src/lib/utilities/pb.js
+++ src/lib/utilities/pb.js
@@ -1,12 +1,12 @@
 import { error } from "@sveltejs/kit"
 
+/**
+ * @param {number} status
+ */
 export function handleOfflineFailure(status) {
-    if (status === 0) {
-        throw error(500, "Database communication failure!")
-    }
+    if (status === 0) throw error(500, "Database communication failure!")
 }
 
 export function getPreviewTierId(tiers) {
-    const tier = tiers.filter(tier => tier.invites === 0 && tier.price === 0)[0]
-    return tier.id
+    return tiers.filter(tier => tier.invites === 0 && tier.price === 0)[0].id
 }

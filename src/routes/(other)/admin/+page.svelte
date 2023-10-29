b1b6da10164260de4f8bd7c51135b3bf16aeead8
babakfp <babak.bxf@gmail.com>
2023-10-29 12:14:31 -0700

    Admin tiers stuff

--- src/routes/(other)/admin/+page.svelte
+++ src/routes/(other)/admin/+page.svelte
@@ -5,29 +5,48 @@
     export let data
 
     let selectedTierIds = []
+
+    function deleteSelectedTiers() {
+        // TODO
+    }
 </script>
 
-<Table>
+{#if selectedTierIds.length}
+    <div class="flex justify-between">
+        <button class="btn btn-gray" on:click={deleteSelectedTiers}>
+            Delete selected
+        </button>
+
+        <button class="btn btn-gray" on:click={() => (selectedTierIds = [])}>
+            Clear selection
+        </button>
+    </div>
+{/if}
+
+<Table class="mt-4">
     <Thead>
         <Tr>
-            <Th>
-                <Checkbox
-                    checked={selectedTierIds.length === data.tiers.length}
-                    on:click={e => {
-                        if (e.target.checked) {
-                            selectedTierIds = data.tiers.map(tier => tier.id)
-                        } else {
-                            selectedTierIds = []
-                        }
-                    }}
-                />
+            <Th class="!p-0">
+                <label class="relative z-1 flex items-center px-6 py-3 pt-3.5">
+                    <Checkbox
+                        checked={selectedTierIds.length === data.tiers.length}
+                        on:click={e => {
+                            if (e.target.checked) {
+                                selectedTierIds = data.tiers.map(
+                                    tier => tier.id,
+                                )
+                            } else {
+                                selectedTierIds = []
+                            }
+                        }}
+                    />
+                </label>
             </Th>
             <Th>TIER</Th>
             <Th>PRICE</Th>
             <Th>INVITES</Th>
             <Th>VIDEOS</Th>
             <Th>USERS</Th>
-            <Th class="text-right">ACTIONS</Th>
         </Tr>
     </Thead>
     <Tbody>
@@ -36,9 +55,9 @@
                 user.retainedTiers.includes(tier.id),
             )}
             <Tr
-                class="group relative duration-200 hover:bg-white/10 not-last:border-b not-last:border-white/5"
+                class="relative duration-200 hover:bg-white/10 not-last:border-b not-last:border-white/5"
             >
-                <Td>
+                <Td class="w-16">
                     <label class="relative z-1 flex items-center px-6 py-4">
                         <Checkbox
                             checked={selectedTierIds.includes(tier.id)}
@@ -72,12 +91,10 @@
                 <Td class="px-6 py-4">
                     {usersWithThisTier.length}
                 </Td>
-
-                <Td class="text-right">
-                    <a class="link px-6 py-4" href="/admin/tiers/{tier.id}">
-                        View
-                    </a>
-                </Td>
+                <a
+                    class="absolute inset-0 outline-inset"
+                    href="/admin/tiers/{tier.id}"
+                />
             </Tr>
         {/each}
     </Tbody>

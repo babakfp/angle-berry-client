eb96ad253fd88691312bd9fc7374ac60dad39c24
babakfp <babak.bxf@gmail.com>
2023-11-02 00:04:01 -0700

    Updated types

--- src/lib/utilities/pb.ts
+++ src/lib/utilities/pb.ts
@@ -1,9 +1,10 @@
 import { error } from "@sveltejs/kit"
+import type { TiersResponse } from "$utilities/pb-types"
 
 export function handleOfflineFailure(status: number) {
     if (status === 0) throw error(500, "Database communication failure!")
 }
 
-export function getPreviewTierId(tiers) {
+export function getPreviewTierId(tiers: TiersResponse[]) {
     return tiers.filter(tier => tier.invites === 0 && tier.price === 0)[0]?.id
 }

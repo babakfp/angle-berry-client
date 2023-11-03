62c97902f03ed168d1d1dbc2c93f261b3b93891a
babakfp <babak.bxf@gmail.com>
2023-11-03 06:07:38 -0700

    Added types

--- src/routes/(other)/+layout.server.ts
+++ src/routes/(other)/+layout.server.ts
@@ -1,6 +1,7 @@
 import { redirect } from "@sveltejs/kit"
 import { pbHandleClientResponseError } from "$utilities/pb"
-import type { ClientResponseError } from "pocketbase"
+import type { ListResult, ClientResponseError } from "pocketbase"
+import type { MessagesResponse, EventsResponse } from "$utilities/pb-types"
 
 export async function load({ locals, parent }) {
     const data = await parent()
@@ -8,14 +9,18 @@ export async function load({ locals, parent }) {
     if (!locals.user) throw redirect(303, "/login")
 
     try {
-        const messages = await locals.pb.collection("messages").getList(1, 50, {
-            sort: "-created",
-            expand: "user,repliedTo,repliedTo.user",
-        })
-        const events = await locals.pb.collection("events").getList(1, 50, {
-            sort: "-created",
-            expand: "user,user.retainedTiers,inviter,inviter.retainedTiers",
-        })
+        const messages: ListResult<MessagesResponse[]> = await locals.pb
+            .collection("messages")
+            .getList(1, 50, {
+                sort: "-created",
+                expand: "user,repliedTo,repliedTo.user",
+            })
+        const events: ListResult<EventsResponse[]> = await locals.pb
+            .collection("events")
+            .getList(1, 50, {
+                sort: "-created",
+                expand: "user,user.retainedTiers,inviter,inviter.retainedTiers",
+            })
         return {
             ...data,
             user: locals.user, // Note: Yes, `locals.user` is not `null` after the `if` statment, however we are uisng `data` and `user` is `null` inside it!

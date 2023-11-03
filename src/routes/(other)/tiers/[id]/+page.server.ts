d8713d2d60d531bc3c445d9ac09499d043da765d
babakfp <babak.bxf@gmail.com>
2023-11-03 01:50:07 -0700

    Added types

--- src/routes/(other)/tiers/[id]/+page.server.ts
+++ src/routes/(other)/tiers/[id]/+page.server.ts
@@ -3,26 +3,23 @@ import type { ClientResponseError } from "pocketbase"
 
 export async function load({ locals, params }) {
     try {
-        const record = await locals.pb.collection("tiers").getOne(params.id, {
-            expand: "videos",
-        })
-        if (record) {
-            const tier = record
-            if (
-                params.id === locals.previewTierId ||
-                locals.user.retainedTiers.includes(params.id) ||
-                locals.user.invitedUsers.length >= tier.invites
-            ) {
-                return {
-                    hasAccessToThisTier: true,
-                    tier: record,
-                }
-            } else {
-                delete tier.file
-                return {
-                    hasAccessToThisTier: false,
-                    tier: record,
-                }
+        const tier = await locals.pb
+            .collection("tiers")
+            .getOne(params.id, { expand: "videos" })
+        if (
+            params.id === locals.previewTierId ||
+            locals.user.retainedTiers.includes(params.id) ||
+            locals.user.invitedUsers.length >= tier.invites
+        ) {
+            return {
+                hasAccessToThisTier: true,
+                tier,
+            }
+        } else {
+            delete tier.file
+            return {
+                hasAccessToThisTier: false,
+                tier,
             }
         }
     } catch (e) {

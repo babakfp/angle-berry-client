26cd52468e219f62a61378438e1c182cdd5f836d
babakfp <babak.bxf@gmail.com>
2023-11-04 16:28:24 -0700

    Better input inc dec button styles

--- src/lib/components/form/InputNumberButtons.svelte
+++ src/lib/components/form/InputNumberButtons.svelte
@@ -36,9 +36,18 @@
     }
 </script>
 
-<button class="h-full px-1" type="button" on:click={incrementNumber}>
+<button
+    class="h-full pl-1 pr-0.5 text-gray-500 duration-200 outline-inset hover:text-gray-300"
+    type="button"
+    on:click={incrementNumber}
+>
     <IconPlusCircle class="text-lg" />
 </button>
-<button class="-mr-1 h-full px-1" type="button" on:click={decrementNumber}>
+
+<button
+    class="-mr-2 h-full pl-0.5 pr-2 text-gray-500 duration-200 outline-inset hover:text-gray-300"
+    type="button"
+    on:click={decrementNumber}
+>
     <IconMinusCircle class="text-lg" />
 </button>

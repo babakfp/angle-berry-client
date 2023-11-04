0b6edc1a211c958ce372a5a5cd4fd998d034427d
babakfp <babak.bxf@gmail.com>
2023-11-04 16:31:37 -0700

    Switched input number buttons order

--- src/lib/components/form/InputNumberButtons.svelte
+++ src/lib/components/form/InputNumberButtons.svelte
@@ -39,15 +39,15 @@
 <button
     class="h-full pl-1 pr-0.5 text-gray-500 duration-200 outline-inset hover:text-gray-300"
     type="button"
-    on:click={incrementNumber}
+    on:click={decrementNumber}
 >
-    <IconPlusCircle class="text-lg" />
+    <IconMinusCircle class="text-lg" />
 </button>
 
 <button
     class="-mr-2 h-full pl-0.5 pr-2 text-gray-500 duration-200 outline-inset hover:text-gray-300"
     type="button"
-    on:click={decrementNumber}
+    on:click={incrementNumber}
 >
-    <IconMinusCircle class="text-lg" />
+    <IconPlusCircle class="text-lg" />
 </button>

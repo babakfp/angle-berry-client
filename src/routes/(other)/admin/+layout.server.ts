cb00ed0693071fa62c93ecaba2df7e87469d330c
babakfp <babak.bxf@gmail.com>
2023-11-03 20:14:31 -0700

    Fixed auth

--- src/routes/(other)/admin/+layout.server.ts
+++ src/routes/(other)/admin/+layout.server.ts
@@ -1,6 +1,8 @@
 import { error } from "@sveltejs/kit"
 
-export async function load({ locals }) {
+export async function load({ locals, parent }) {
+    await parent()
+
     if (!locals.user.isAdmin)
         throw error(401, "You are not authorized to see this page!")
 }

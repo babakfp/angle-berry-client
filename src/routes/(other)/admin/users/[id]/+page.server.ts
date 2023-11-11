6183151d21974ba901e3715255072701ad3d7270
babakfp <babak.bxf@gmail.com>
2023-11-11 14:37:31 -0800

    Added /admin/users/[id]

--- src/routes/(other)/admin/users/[id]/+page.server.ts
+++ src/routes/(other)/admin/users/[id]/+page.server.ts
@@ -4,7 +4,11 @@ import {
     pbHandleFormActionError,
 } from "$utilities/pb"
 import { superValidate } from "sveltekit-superforms/server"
-import type { UsersResponse, ClientResponseError } from "$utilities/pb-types"
+import type {
+    UsersResponse,
+    TiersResponse,
+    ClientResponseError,
+} from "$utilities/pb-types"
 import { schema } from "./schema"
 
 export async function load({ locals, params }) {
@@ -18,30 +22,33 @@ export async function load({ locals, params }) {
         const user: UsersResponse = await locals.pb
             .collection("users")
             .getOne(params.id)
-        return { form, user }
+        const tiers: TiersResponse[] = await locals.pb
+            .collection("tiers")
+            .getFullList()
+        return { form, user, tiers }
     } catch (e) {
         pbHandleClientResponseError(e as ClientResponseError)
         throw e
     }
 }
 
-// export const actions = {
-//     default: async ({ locals, request, params }) => {
-//         if (!locals.user) throw redirect(303, "/login")
-//         if (!locals.user.isAdmin)
-//             throw error(401, "You are not authorized to see this page!")
+export const actions = {
+    default: async ({ locals, request, params }) => {
+        if (!locals.user) throw redirect(303, "/login")
+        if (!locals.user.isAdmin)
+            throw error(401, "You are not authorized to see this page!")
 
-//         const form = await superValidate(request, schema)
-//         if (!form.valid) return fail(400, { form })
+        const form = await superValidate(request, schema)
+        if (!form.valid) return fail(400, { form })
 
-//         try {
-//             await locals.pb.collection("tiers").update(params.id, form.data)
-//         } catch (e) {
-//             const e2 = pbHandleFormActionError(e, form)
-//             if (e2) return e2
-//             throw e
-//         }
+        try {
+            await locals.pb.collection("users").update(params.id, form.data)
+        } catch (e) {
+            const e2 = pbHandleFormActionError(e, form)
+            if (e2) return e2
+            throw e
+        }
 
-//         throw redirect(303, "/admin/tiers")
-//     },
-// }
+        throw redirect(303, "/admin/users")
+    },
+}
--- src/routes/(other)/admin/users/[id]/+page.svelte
+++ src/routes/(other)/admin/users/[id]/+page.svelte
@@ -3,6 +3,9 @@
     import { superForm } from "sveltekit-superforms/client"
     import Form from "$components/form/Form.svelte"
     import Checkbox from "$components/form/Checkbox.svelte"
+    import Select from "svelte-select"
+    import IconX from "$icons/IconX.svelte"
+    import IconXCircle from "$icons/IconXCircle.svelte"
 
     export let data
     export let form
@@ -15,8 +18,14 @@
     } = superForm(data.form, { validators: schema })
 
     if (!$_form.isAdmin) $_form.isAdmin = data.user.isAdmin
+    if (!$_form.retainedTiers.length)
+        $_form.retainedTiers = data.user.retainedTiers
 
-    // $: selectedVideos = data.videos.filter(v => $_form.videos.includes(v.id))
+    let selectedRetainedTiers = data.tiers
+        .filter(tier => $_form.retainedTiers.includes(tier.id))
+        .map(tier => ({ value: tier.id, label: tier.name }))
+
+    $: $_form.retainedTiers = selectedRetainedTiers.map(tier => tier.value)
 </script>
 
 <svelte:head>
@@ -31,6 +40,51 @@
         {errors}
         {validate}
     >
+        <div class="bg-gray-600"></div>
+        <Select
+            items={data.tiers.map(tier => ({
+                value: tier.id,
+                label: tier.name,
+            }))}
+            value={selectedRetainedTiers}
+            multiple={true}
+            on:input={e => {
+                selectedRetainedTiers = e.detail || []
+            }}
+            --max-height="2.75rem"
+            --background="rgb(39 39 42)"
+            --multi-select-padding="0 .5rem"
+            --border-radius="0.375rem"
+            --border="2px solid rgb(63 63 70)"
+            --border-hover="2px solid rgb(63 63 70)"
+            --border-focused="2px solid rgb(63 63 70)"
+            --value-container-padding="0"
+            --multi-item-bg="rgb(63 63 70)"
+            --multi-item-outline="none"
+            --multi-item-padding="0 0.25rem 0 0.5rem"
+            --clear-select-width="32px"
+            --clear-select-margin="0 -0.5rem 0 0"
+            --list-background="rgb(63 63 70)"
+            --item-hover-bg="rgb(82 82 91)"
+        >
+            <svelte:fragment slot="clear-icon">
+                <IconX class="text-lg" />
+            </svelte:fragment>
+            <svelte:fragment slot="multi-clear-icon">
+                <IconXCircle />
+            </svelte:fragment>
+        </Select>
+
+        {#each $_form.retainedTiers as t}
+            <input
+                class="hidden"
+                type="checkbox"
+                checked
+                name="retainedTiers"
+                value={t}
+            />
+        {/each}
+
         <label class="inline-flex gap-2">
             <Checkbox
                 bind:checked={$_form.isAdmin}
@@ -41,3 +95,12 @@
         </label>
     </Form>
 </div>
+
+<style lang="postcss">
+    :global(.multi-item-text) {
+        @apply inline-flex items-center text-sm;
+    }
+    :global(.value-container) {
+        @apply !gap-2;
+    }
+</style>

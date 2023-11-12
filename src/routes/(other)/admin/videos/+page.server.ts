04b274645a328fcdb26c090f2892760c1fc9d790
babakfp <babak.bxf@gmail.com>
2023-11-12 12:59:37 -0800

    Stuff /admin/videos

--- src/routes/(other)/admin/videos/+page.server.ts
+++ src/routes/(other)/admin/videos/+page.server.ts
@@ -1,8 +1,11 @@
 import { redirect, error } from "@sveltejs/kit"
-import { pbHandleClientResponseError } from "$utilities/pb"
+import {
+    pbHandleClientResponseError,
+    pbHandleFormActionError,
+} from "$utilities/pb"
 import type { VideosResponse, ClientResponseError } from "$utilities/pb-types"
 
-export async function load({ locals, params }) {
+export async function load({ locals }) {
     if (!locals.user) throw redirect(303, "/login")
     if (!locals.user.isAdmin)
         throw error(401, "You are not authorized to see this page!")
@@ -16,4 +19,41 @@ export async function load({ locals, params }) {
         pbHandleClientResponseError(e as ClientResponseError)
         throw e
     }
+}
+
+export const actions = {
+    default: async ({ locals, request }) => {
+        if (!locals.user) throw redirect(303, "/login")
+        if (!locals.user.isAdmin)
+            throw error(401, "You are not authorized to see this page!")
+
+        const formData = await request.formData()
+
+        const videos = formData.getAll("videos")
+
+        // const form = await superValidate(request, tierDeletionSchema)
+        // if (!form.valid) return fail(400, { form })
+
+        try {
+            console.log("videos", videos)
+
+            await Promise.all(
+                videos.map(video => {
+                    console.log("video", video)
+
+                    return locals.pb
+                        .collection("videos")
+                        .create({ file: video }, { requestKey: null })
+                }),
+            )
+        } catch (e) {
+            console.log("e", e)
+
+            console.log(JSON.stringify(e, null, 4))
+
+            const e2 = pbHandleFormActionError(e, {})
+            if (e2) return e2
+            throw e
+        }
+    },
 }
--- src/routes/(other)/admin/videos/+page.svelte
+++ src/routes/(other)/admin/videos/+page.svelte
@@ -5,6 +5,18 @@
     export let data
 </script>
 
+<form method="post" enctype="multipart/form-data">
+    <label for="file">Choose file to upload</label>
+    <input
+        type="file"
+        name="videos"
+        accept=".mp4,.avi,.mkv"
+        id="videos"
+        multiple
+    />
+    <button type="submit">Submit</button>
+</form>
+
 <ul class="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
     {#each data.videos as video}
         <li>

bf38bf8c785bc9f8aa72dffa1ca2a4e7d251ad75
babakfp <babak.bxf@gmail.com>
2023-12-01 19:23:34 -0800

    Updated Select styles

--- src/routes/(other)/admin/users/[id]/+page.svelte
+++ src/routes/(other)/admin/users/[id]/+page.svelte
@@ -51,21 +51,26 @@
             on:input={e => {
                 selectedRetainedTiers = e.detail || []
             }}
-            --max-height="2.75rem"
+            placeholder="Add tiers"
+            placeholderAlwaysShow={$_form.retainedTiers.length !==
+                data.tiers.length}
+            --height="2.75rem"
             --background="rgb(39 39 42)"
             --multi-select-padding="0 .5rem"
             --border-radius="0.375rem"
             --border="2px solid rgb(63 63 70)"
             --border-hover="2px solid rgb(63 63 70)"
             --border-focused="2px solid rgb(63 63 70)"
-            --value-container-padding="0"
+            --value-container-padding="0.5rem 0"
             --multi-item-bg="rgb(63 63 70)"
             --multi-item-outline="none"
-            --multi-item-padding="0 0.25rem 0 0.5rem"
+            --multi-item-padding="0 0.5rem 0 0"
             --clear-select-width="32px"
             --clear-select-margin="0 -0.5rem 0 0"
             --list-background="rgb(63 63 70)"
             --item-hover-bg="rgb(82 82 91)"
+            --multi-select-input-margin="0"
+            --multi-item-gap="0"
         >
             <svelte:fragment slot="clear-icon">
                 <IconX class="text-lg" />
@@ -100,5 +105,16 @@
     }
     :global(.value-container) {
         @apply !gap-2;
+    }
+    :global(.svelte-select) {
+        --placeholder-color: theme(colors.gray.600);
+        --list-empty-color: theme(colors.gray.500);
+        --font-size: theme(fontSize.sm);
+    }
+    :global(.svelte-select .multi-item-clear) {
+        @apply cursor-pointer px-1 text-base hover:text-white;
+    }
+    :global(.svelte-select .multi-item-text) {
+        @apply order-2;
     }
 </style>

74f1cb0ee76f7e831977eceb9b82be6c7f09539f
babakfp <babak.bxf@gmail.com>
2024-01-23 04:34:37 -0800

    Fixed NotificationBlob width

--- src/lib/components/NotificationBlob.svelte
+++ src/lib/components/NotificationBlob.svelte
@@ -1,5 +1,5 @@
 <div
-    class="absolute right-2 top-4 h-3.5 w-3.5 rounded-full bg-gray-600 pt-[3px] text-2xs"
+    class="absolute right-2 top-4 h-3.5 min-w-3.5 rounded-full bg-gray-600 px-1 pt-[3px] text-2xs"
 >
     <slot />
 </div>

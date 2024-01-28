e89a871ba5f0883058e6e355d31075d555f59b7e
babakfp <babak.bxf@gmail.com>
2024-01-28 01:06:29 -0800

    Hide Modal with CSS not JS

--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -11,32 +11,31 @@
     }}
 />
 
-{#if isOpen}
+<div
+    class="fixed inset-0 z-50 flex h-screen w-screen items-center justify-center bg-body-bg/50
+        {isFullSize ? 'p-0 sm:p-8' : 'p-8'}
+        {!isOpen && 'hidden'}"
+    on:click|self={() => (isOpen = false)}
+>
     <div
-        class="fixed inset-0 z-50 flex h-screen w-screen items-center justify-center bg-body-bg/50
-            {isFullSize ? 'p-0 sm:p-8' : 'p-8'}"
-        on:click|self={() => (isOpen = false)}
-    >
-        <div
-            class="grid gap-6 overflow-y-auto overscroll-y-contain bg-gray-700 drop-shadow
+        class="grid gap-6 overflow-y-auto overscroll-y-contain bg-gray-700 drop-shadow
                 {isFullSize ? 'h-full w-full sm:rounded' : 'max-w-md rounded'}"
+    >
+        <div class="grid gap-4 p-6 pb-0">
+            {#if title}
+                <h6 class="text-lg">{title}</h6>
+            {/if}
+            {#if description}
+                <p>{description}</p>
+            {/if}
+
+            <slot />
+        </div>
+
+        <div
+            class="sticky bottom-0 flex justify-end gap-2 self-end border-t border-gray-600 bg-gray-700 px-6 py-4"
         >
-            <div class="grid gap-4 p-6 pb-0">
-                {#if title}
-                    <h6 class="text-lg">{title}</h6>
-                {/if}
-                {#if description}
-                    <p>{description}</p>
-                {/if}
-
-                <slot />
-            </div>
-
-            <div
-                class="sticky bottom-0 flex justify-end gap-2 self-end border-t border-gray-600 bg-gray-700 px-6 py-4"
-            >
-                <slot name="actions" />
-            </div>
+            <slot name="actions" />
         </div>
     </div>
-{/if}
+</div>

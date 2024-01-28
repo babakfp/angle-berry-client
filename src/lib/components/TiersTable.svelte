318743636afd5670097a561bb7f004de5eb2964f
babakfp <babak.bxf@gmail.com>
2024-01-28 10:18:35 -0800

    Fixed types and default values

--- src/lib/components/TiersTable.svelte
+++ src/lib/components/TiersTable.svelte
@@ -3,8 +3,8 @@
     import { Table, Tbody, Thead, Tr, Th } from "$components/table/index"
     import TiersTableRow from "$components/TiersTableRow.svelte"
 
-    export let tiers: TiersResponse[] = []
-    export let currentTier: TiersResponse | null = null
+    export let tiers: TiersResponse[]
+    export let currentTier: TiersResponse | undefined = undefined
     export let _class = ""
     export { _class as class }
 </script>

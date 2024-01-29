89c8ab3c71a026e547789fdfcef95b1b69af32cf
babakfp <babak.bxf@gmail.com>
2024-01-29 15:32:11 -0800

    Fixed types in app.d.ts

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,19 +1,28 @@
-import type PocketBase from "pocketbase"
-import type { UsersResponse } from "$utilities/pb-types"
+import type PocketBase, { ListResult } from "pocketbase"
+import type {
+    UsersResponse,
+    RealtimeMessagesResponse,
+    RealtimeEventsResponse,
+    TiersResponse,
+} from "$utilities/pb-types"
 
 declare global {
     namespace App {
         interface Locals {
             pb: PocketBase
             user?: UsersResponse
-            tiers: TiersResponse[]
             previewTierId?: string
+            messages: ListResult<RealtimeMessagesResponse>
+            events: ListResult<RealtimeEventsResponse>
+            tiers: TiersResponse[]
         }
         interface PageData {
             // TODO: I don't know how to specify that the type can be `undefined` inside `(auth)` routes and ensure that it will be available inside `(other)` routes.
             user: UsersResponse // Is `undefined` inside `(auth)` routes.
-            tiers: TiersResponse[]
             previewTierId?: string
+            messages: ListResult<RealtimeMessagesResponse>
+            events: ListResult<RealtimeEventsResponse>
+            tiers: TiersResponse[]
         }
     }
 }

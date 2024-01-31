26b17856b33b843a8c3e3616b3b64b62b4caffb5
babakfp <babak.bxf@gmail.com>
2024-01-31 15:39:43 -0800

    Added JSDocs comment to Svelte config file

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import adapter from "@sveltejs/adapter-vercel"
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
 
+/** @type {import("@sveltejs/kit").Config} */
 export default {
     kit: {
         adapter: adapter(),

cc02c2b279c071b5609c1fb1bf32569a7d63d34d
babakfp <babak.bxf@gmail.com>
2024-02-13 14:46:20 -0800

    Formatted

--- src/routes/(other)/+page.svelte
+++ src/routes/(other)/+page.svelte
@@ -25,16 +25,18 @@
 </TiersTable>
 
 <div class="mt-8">
-    <!-- prettier-ignore -->
     <p>
-		You can get the premium tiers by <a class="link" href="/how-to-pay">paying</a> or
-		<a class="link" href="/how-to-invite">inviting</a> new users to this website.
-	</p>
+        You can get the premium tiers by
+        <a class="link" href="/how-to-pay">paying</a>
+        or
+        <a class="link" href="/how-to-invite">inviting</a>
+        new users to this website.
+    </p>
 
     <p>
         You can contact us via Telegram
         <a class="link" href="https://t.me/adminTelegramUsername">
-            @adminTelegramUsername
-        </a>.
+            @adminTelegramUsername</a
+        >.
     </p>
 </div>

5afbe6c84d7543431036aeea8797922a928f4c7b
babakfp <babak.bxf@gmail.com>
2024-02-13 20:42:04 -0800

    Formatted

--- src/routes/(other)/admin/+page.server.ts
+++ src/routes/(other)/admin/+page.server.ts
@@ -4,5 +4,6 @@ export const load = async ({ locals }) => {
     if (!locals.user) redirect(303, "/login")
     if (!locals.user.isAdmin)
         error(401, "You are not authorized to see this page!")
+
     return {}
 }

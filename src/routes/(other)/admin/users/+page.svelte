cc4589283358b816219f990f9d89c53722e07fda
babakfp <babak.bxf@gmail.com>
2024-02-17 20:01:41 -0800

    Removed checkboxes from users table

--- src/routes/(other)/admin/users/+page.svelte
+++ src/routes/(other)/admin/users/+page.svelte
@@ -5,60 +5,20 @@
         Thead,
         Tr,
         Th,
-        ThCheckbox,
         Td,
         TrOverlayAnchor,
     } from "$components/table/index"
-    import Checkbox from "$components/form/Checkbox.svelte"
 
     export let data
-
-    let selectedUserIds: string[] = []
-
-    const deleteSelectedTiers = () => {}
-
-    const checkAllCheckboxes = (e: Event) => {
-        if ((e.target as HTMLInputElement).checked) {
-            selectedUserIds = data.users.map(user => user.id)
-        } else {
-            selectedUserIds = []
-        }
-    }
 </script>
 
 <svelte:head>
     <title>Users</title>
 </svelte:head>
 
-{#if selectedUserIds.length}
-    <div class="flex justify-between">
-        <button
-            type="button"
-            class="btn btn-danger"
-            on:click={deleteSelectedTiers}
-        >
-            Delete selected
-        </button>
-
-        <button
-            type="button"
-            class="btn btn-gray-light"
-            on:click={() => (selectedUserIds = [])}
-        >
-            Clear selection
-        </button>
-    </div>
-{/if}
-
 <Table class="mt-4">
     <Thead>
         <Tr>
-            <ThCheckbox
-                checked={!!data.tiers.length &&
-                    selectedUserIds.length === data.users.length}
-                readonly={!data.tiers.length}
-                on:change={checkAllCheckboxes}
-            />
             <Th>USERNAME</Th>
             <Th>TIERS</Th>
             <Th>INVITES</Th>
@@ -70,14 +30,6 @@
             <Tr
                 class="relative duration-200 hover:bg-white/10 not-last:border-b not-last:border-white/5"
             >
-                <Td class="w-16">
-                    <Checkbox
-                        class="relative z-1 px-6 py-4"
-                        checked={selectedUserIds.includes(user.id)}
-                        bind:group={selectedUserIds}
-                        value={user.id}
-                    />
-                </Td>
                 <Th class="py-4 text-white">
                     {user.username}
                 </Th>

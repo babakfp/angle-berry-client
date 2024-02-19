01ed253f20bd3ff130b1d5fa3db0007cbee843a9
babakfp <babak.bxf@gmail.com>
2024-02-18 19:24:07 -0800

    Added toggle to FloatingActions

--- src/lib/components/table/FloatingActions.svelte
+++ src/lib/components/table/FloatingActions.svelte
@@ -1,16 +1,37 @@
 <script lang="ts">
+    import IconArrowUpRegular from "phosphor-icons-svelte/IconArrowUpRegular.svelte"
+    import IconArrowDownRegular from "phosphor-icons-svelte/IconArrowDownRegular.svelte"
+
     export let selectedItemIds: string[]
     export let deleteActionAttribute: `?/${string}` | undefined
     export let deleteInputNameAttribute: string
+
+    let isPartlyVisible = false
+
+    $: if (!selectedItemIds.length) {
+        isPartlyVisible = false
+    }
 </script>
 
 <div
     class="fixed inset-x-0 bottom-4 z-50 px-4 duration-200 sm:bottom-8 sm:mx-auto sm:max-w-xl
-    {!selectedItemIds.length && 'translate-y-full hide'}"
+    {!selectedItemIds.length && 'translate-y-full hide'}
+    {isPartlyVisible && 'translate-y-full sm:translate-y-[calc(100%+1rem)]'}"
 >
     <div
-        class="grid gap-2 rounded bg-gray-700 p-4 sm:flex sm:items-center sm:justify-between"
+        class="group grid gap-2 rounded bg-gray-700 p-4 sm:flex sm:items-center sm:justify-between"
     >
+        <button
+            class="btn btn-gray-light absolute -top-4 h-8 w-8 rounded-full p-0 duration-200 hide inset-x-center group-hover:show"
+            on:click={() => (isPartlyVisible = !isPartlyVisible)}
+        >
+            {#if isPartlyVisible}
+                <IconArrowUpRegular />
+            {:else}
+                <IconArrowDownRegular />
+            {/if}
+        </button>
+
         <span class="text-center sm:order-2">
             Selected <b>{selectedItemIds.length}</b>
             {selectedItemIds.length > 1 ? "items" : "item"}

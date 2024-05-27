02307422c0135902127cfbb583d5e46f983b1b40
Babak Farkhoopak <44144724+babakfp@users.noreply.github.com>
2024-05-27 04:19:25 +0330

    Fixed spacing

--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -10,14 +10,14 @@
 </svelte:head>
 
 {#if data.previewTierId}
-    <p>
+    <p class="mb-8">
         Watch <a class="link" href="/tiers/{data.previewTierId}">free</a>
         content, and <a class="link" href="/how-to-pay">pay</a> or
         <a class="link" href="/how-to-invite">invite</a> to access premium tiers.
     </p>
 {/if}
 
-<TiersTable class="mt-8">
+<TiersTable>
     {#each data.tiers as tier}
         <TiersTableRow loggedInUser={data.loggedInUser} {tier} />
     {/each}

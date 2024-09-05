a53c37aa58eb3f14a374f126b52023f37bc69a87
babakfp <babak.bxf@gmail.com>
2024-09-05 09:44:25 +0330

    Updated error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -6,26 +6,24 @@
     <title>{$page.status}</title>
 </svelte:head>
 
-<div
-    class="mx-auto grid h-screen max-w-xs content-center items-center text-center"
->
-    <h1 class="text-8xl font-bold text-gray-50">
+<main class="mx-auto grid h-screen max-w-xs content-center gap-4 text-center">
+    <h1 class="text-gray-50 text-8xl font-bold">
         {$page.status}
     </h1>
 
-    <div class="text-gray-400">
-        {#if $page.status === 404}
-            {#if $page.error?.message}
-                <p>{$page.error.message}</p>
-            {:else}
-                <p>The page that you are looking for couldn't be found!</p>
-            {/if}
-        {:else if $page.error}
-            <p>{$page.error.message}</p>
+    {#if $page.status === 404}
+        {#if $page.error?.message}
+            <p class="text-gray-400">{$page.error.message}</p>
+        {:else}
+            <p class="text-gray-400">
+                The page that you are looking for couldn't be found!
+            </p>
         {/if}
+    {:else if $page.error}
+        <p class="text-gray-400">{$page.error.message}</p>
+    {/if}
 
-        {#if $page.status === 404 || $page.status === 401}
-            <a class="link btn btn-gray-light mt-4" href="/">Go home</a>
-        {/if}
-    </div>
-</div>
+    {#if $page.status === 404 || $page.status === 401}
+        <a class="link btn btn-gray-light-glass" href="/">Go home</a>
+    {/if}
+</main>

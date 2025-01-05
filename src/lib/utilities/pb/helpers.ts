2e9494938736f3c3abd36aec6d9b02b75ac70f6d
babakfp <babak.bxf@gmail.com>
2025-01-06 00:58:35 +0330

    chore: remove `isErrorConnectionRefused`

--- src/lib/utilities/pb/helpers.ts
+++ src/lib/utilities/pb/helpers.ts
@@ -1,34 +1,31 @@
 import { error, fail } from "@sveltejs/kit"
 import type { SuperValidated } from "sveltekit-superforms"
 import { ClientResponseError } from "$lib/utilities/pb"
-import { isErrorConnectionRefused } from "./errorConnectionRefused"
 
 export const pbHandleFormActionError = (
     e: unknown | ClientResponseError,
     formData: SuperValidated<Record<string, unknown>> | Record<string, never>,
 ) => {
-    if (isErrorConnectionRefused(e)) {
-        return fail(500, {
-            ...formData,
-            message: "Database communication failure!",
-        })
-    }
+    if (e instanceof ClientResponseError) {
+        if (e.status === 0) {
+            return fail(500, {
+                ...formData,
+                message: "Database communication failure!",
+            })
+        }
 
-    if (
-        e instanceof ClientResponseError &&
-        e.response.code &&
-        e.response.message
-    ) {
-        return fail(e.response.code, {
-            ...formData,
-            message: e.response.message,
-            pb: e.response.data,
-        })
+        if (e.response.code && e.response.message) {
+            return fail(e.response.code, {
+                ...formData,
+                message: e.response.message,
+                pb: e.response.data,
+            })
+        }
     }
 }
 
 export const pbHandleClientResponseError = (e: ClientResponseError) => {
-    if (isErrorConnectionRefused(e)) {
+    if (e.status === 0) {
         error(500, {
             type: "failure",
             message: "Database communication failure!",

0aa70ed665082b3f8bd8b1882c97ccc084392314
babakfp <babak.bxf@gmail.com>
2025-09-16 00:12:36 +0330

    tooltip

--- src/lib/components/Tooltip.svelte
+++ src/lib/components/Tooltip.svelte
@@ -1,144 +1,27 @@
 <script lang="ts">
+    import { Popover } from "@ark-ui/svelte/popover"
     import type { Snippet } from "svelte"
 
     let {
-        position = $bindable("top"),
-        isVisible,
-        keepAlive,
-        parentElementQuerySelector,
-        children,
+        positioning,
+        TriggerSnippet,
+        ContentSnippet,
     }: {
-        position?: string
-        isVisible?: boolean
-        keepAlive?: boolean
-        parentElementQuerySelector?: string
-        children?: Snippet
+        positioning?: Popover.RootProps["positioning"]
+        TriggerSnippet?: Snippet
+        ContentSnippet?: Snippet
     } = $props()
-
-    let tooltip = $state<HTMLDivElement>()
-
-    $effect(() => {
-        if (tooltip && parentElementQuerySelector && isVisible) {
-            const parentElement = document.querySelector(
-                parentElementQuerySelector,
-            )
-
-            const rootElementRect = tooltip.getBoundingClientRect()
-            const parentElementRect = parentElement!.getBoundingClientRect()
-            let isTopNotFullyVisible = false
-            let isBottomNotFullyVisible = false
-
-            if (rootElementRect.top < parentElementRect.top) {
-                isTopNotFullyVisible = true
-            }
-            if (rootElementRect.bottom > parentElementRect.bottom) {
-                isBottomNotFullyVisible = true
-            }
-
-            if (isTopNotFullyVisible) {
-                if (position.includes("-")) {
-                    position = position.replace("top", "bottom")
-                } else {
-                    position = "bottom"
-                }
-            } else if (isBottomNotFullyVisible) {
-                if (position.includes("-")) {
-                    position = position.replace("bottom", "top")
-                } else {
-                    position = "top"
-                }
-            }
-        }
-    })
 </script>
 
-<div
-    bind:this={tooltip}
-    class={[
-        "hide group-hover:show absolute z-50 rounded bg-gray-700 px-4 py-3 text-xs whitespace-nowrap drop-shadow duration-200",
-        { show: isVisible },
-        "after:absolute after:block after:h-0 after:w-0 after:border-solid after:border-transparent",
-        "before:absolute before:block",
-        {
-            "before:inset-x-0 before:h-2 before:w-full": [
-                "top",
-                "top-right",
-                "top-left",
-                "bottom",
-                "bottom-right",
-                "bottom-left",
-            ].includes(position),
-            "before:inset-y-0 before:h-full before:w-2": [
-                "right",
-                "right-top",
-                "right-bottom",
-                "left",
-                "left-top",
-                "left-bottom",
-            ].includes(position),
-
-            "before:[all:unset]": !keepAlive,
-
-            "arrow-bottom bottom-full -translate-y-4 group-hover:-translate-y-2 before:top-full after:top-full":
-                ["top", "top-right", "top-left"].includes(position),
-            "arrow-left left-full translate-x-4 group-hover:translate-x-2 before:right-full after:right-full":
-                ["right", "right-top", "right-bottom"].includes(position),
-            "arrow-top top-full translate-y-4 group-hover:translate-y-2 before:bottom-full after:bottom-full":
-                ["bottom", "bottom-right", "bottom-left"].includes(position),
-            "arrow-right right-full -translate-x-4 group-hover:-translate-x-2 before:left-full after:left-full":
-                ["left", "left-top", "left-bottom"].includes(position),
-
-            "inset-x-center after:inset-x-center":
-                position === "top" || position === "bottom",
-            "inset-y-center after:inset-y-center":
-                position === "right" || position === "left",
-
-            "right-0 after:right-4": ["top-right", "bottom-right"].includes(
-                position,
-            ),
-            "left-0 after:left-4": ["top-left", "bottom-left"].includes(
-                position,
-            ),
-
-            "top-0 after:top-4": ["right-top", "left-top"].includes(position),
-            "bottom-0 after:bottom-4": ["right-bottom", "left-bottom"].includes(
-                position,
-            ),
-        },
-    ]}
->
-    {@render children?.()}
-</div>
-
-<style>
-    @reference "../app.css";
-
-    :root {
-        --arrow-color: var(--color-gray-700);
-        --arrow-size: var(--spacing-1);
-    }
-    .arrow-top::after {
-        border-left-width: var(--arrow-size);
-        border-right-width: var(--arrow-size);
-        border-bottom-width: var(--arrow-size);
-        border-bottom-color: var(--arrow-color);
-    }
-    .arrow-bottom::after {
-        border-left-width: var(--arrow-size);
-        border-right-width: var(--arrow-size);
-        border-top-width: var(--arrow-size);
-        border-top-color: var(--arrow-color);
-    }
-    .arrow-right::after {
-        border-top-width: var(--arrow-size);
-        border-bottom-width: var(--arrow-size);
-        border-left-width: var(--arrow-size);
-        border-left-color: var(--arrow-color);
-    }
-    .arrow-left::after {
-        border-top-width: var(--arrow-size);
-        border-bottom-width: var(--arrow-size);
-        border-right-width: var(--arrow-size);
-        border-right-color: var(--arrow-color);
-    }
-</style>
+<Popover.Root {positioning}>
+    <Popover.Trigger>
+        {@render TriggerSnippet?.()}
+    </Popover.Trigger>
+    <Popover.Positioner>
+        <Popover.Content
+            class="rounded bg-gray-700 px-4 py-3 text-xs whitespace-nowrap drop-shadow duration-200"
+        >
+            {@render ContentSnippet?.()}
+        </Popover.Content>
+    </Popover.Positioner>
+</Popover.Root>
--- src/lib/parts/Header/EventsMenu/EventsMenu.svelte
+++ src/lib/parts/Header/EventsMenu/EventsMenu.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
+    import { fly } from "svelte/transition"
     import PopSide from "$lib/components/PopSide.svelte"
+    import Tooltip from "$lib/components/Tooltip.svelte"
     import { events, unseenEventsLength } from "$lib/stores/events.svelte"
     import type {
         ListResult,
         RealtimeEventsResponse,
+        TiersResponse,
         UsersResponse,
     } from "$lib/utilities/pb"
-    import Event from "./Event.svelte"
 
     let {
         pbEvents,
@@ -27,19 +29,99 @@
             unseenEventsLength._ = 0
         }
     })
+
+    let ref = $state<HTMLDivElement>()
 </script>
 
 <PopSide id="EventsMenu" bind:isOpen {toggleButton}>
-    {#if events._ && events._.items.length > 0}
-        <ol class="overflow-y-auto overscroll-y-contain sm:text-sm">
-            {#each events._.items as event (event.id)}
-                <Event {loggedInUser} {event} />
-            {/each}
-            <p class="p-4 text-center text-xs text-gray-500">
-                No more events found!
-            </p>
-        </ol>
-    {:else}
-        <p class="px-4 py-6 text-center">No events found!</p>
-    {/if}
+    <div bind:this={ref}>
+        {#if events._ && events._.items.length > 0}
+            <ol class="overflow-y-auto overscroll-y-contain sm:text-sm">
+                {#each events._.items as event (event.id)}
+                    {@render Event({ event })}
+                {/each}
+                <p class="p-4 text-center text-xs text-gray-500">
+                    No more events found!
+                </p>
+            </ol>
+        {:else}
+            <p class="px-4 py-6 text-center">No events found!</p>
+        {/if}
+    </div>
 </PopSide>
+
+{#snippet Event({ event }: { event: RealtimeEventsResponse })}
+    <li
+        class="border-b border-gray-50/5 p-4"
+        transition:fly={{ x: 64, duration: 500 }}
+    >
+        {#if !event.expand?.inviter}
+            {#if event.expand.user.id === loggedInUser.id}
+                <div>
+                    Welcome. You {@render UserTooltipInfo({
+                        user: event.expand.user,
+                    })} have successfully joined this fantastic website. We hope
+                    you have a fabulous time here.
+                </div>
+            {:else}
+                <div>
+                    User {@render UserTooltipInfo({ user: event.expand.user })} has
+                    successfully joined this website.
+                </div>
+            {/if}
+        {:else if event.expand.user.id === loggedInUser.id}
+            <div>
+                Welcome. You {@render UserTooltipInfo({
+                    user: event.expand.user,
+                })} have successfully joined this fantastic website. We hope you
+                have a fabulous time here. User {@render UserTooltipInfo({
+                    user: event.expand.inviter,
+                })} has invited you.
+            </div>
+        {:else}
+            <div>
+                User {@render UserTooltipInfo({ user: event.expand.inviter })} has
+                invited
+                {@render UserTooltipInfo({ user: event.expand.user })} to this website.
+            </div>
+        {/if}
+    </li>
+{/snippet}
+
+{#snippet UserTooltipInfo({
+    user,
+}: {
+    user: UsersResponse & {
+        expand: {
+            retainedTiers: TiersResponse[]
+        }
+    }
+})}
+    <Tooltip
+        positioning={{
+            boundary: ref,
+            placement: "top-start",
+        }}
+    >
+        {#snippet TriggerSnippet()}
+            <span class="rounded-full bg-gray-700 px-1 py-px">
+                {user.username}
+            </span>
+        {/snippet}
+
+        {#snippet ContentSnippet()}
+            <ul class="list-inside list-disc space-y-1">
+                <li>Invites: {user.invitedUsers.length}</li>
+                <li>
+                    Tiers:
+                    {#each user.expand.retainedTiers as tier, i}
+                        <a class="link" href="/tiers/{tier.id}">
+                            {tier.name}
+                        </a>
+                        {user.expand.retainedTiers.length - 1 !== i ? " " : ""}
+                    {/each}
+                </li>
+            </ul>
+        {/snippet}
+    </Tooltip>
+{/snippet}

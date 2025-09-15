ee425f3448e6563795177c1023b5df7bb1559cdb
babakfp <babak.bxf@gmail.com>
2025-09-16 03:25:50 +0330

    /

--- src/lib/parts/Header/UserMenu/UserMenu.svelte
+++ src/lib/parts/Header/UserMenu/UserMenu.svelte
@@ -46,7 +46,7 @@
     </Popover.Trigger>
     <Popover.Positioner>
         <Popover.Content
-            class="bg-background hide data-[state=open]:show relative w-64 translate-x-6 rounded text-sm shadow-[0_4px_16px_0_rgb(0_0_0/0.4)] duration-150 data-[state=open]:-translate-x-2"
+            class="bg-background hide data-[state=open]:show relative w-64 translate-x-6 overflow-hidden rounded text-sm shadow-[0_4px_16px_0_rgb(0_0_0/0.4)] duration-150 data-[state=open]:-translate-x-2"
             hidden={false}
         >
             {@render ScrollShadow({ position: "top" })}

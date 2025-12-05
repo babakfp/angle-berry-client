a5452fcc53ffa712735f6f03616924f49f17ba9b
babakfp <babak.bxf@gmail.com>
2025-12-05 22:58:43 +0330

    use state instead of events

--- src/lib/components/Video.svelte
+++ src/lib/components/Video.svelte
@@ -1,35 +1,21 @@
-<script lang="ts">
-    import type { EventHandler, MouseEventHandler } from "svelte/elements"
+<script module lang="ts">
+    let volume = $state(0.25)
+</script>
 
+<script lang="ts">
     let {
         src,
     }: {
         src: string
     } = $props()
-
-    // When 1 video player volume changes, make other video player volume change too.
-    const handleVolumeChange: EventHandler<Event, HTMLVideoElement> = (e) => {
-        document.querySelectorAll("video").forEach((videoElement) => {
-            videoElement.volume = (e.target as HTMLVideoElement).volume
-        })
-    }
-
-    const handleLoadStart: EventHandler<Event, HTMLVideoElement> = (e) => {
-        e.currentTarget.volume = 0.25
-    }
-
-    const handleContextMenu: MouseEventHandler<HTMLVideoElement> = (e) => {
-        e.preventDefault()
-    }
 </script>
 
 <video
-    onvolumechange={handleVolumeChange}
     class="aspect-video w-full rounded"
     {src}
     controls
-    onloadstart={handleLoadStart}
     controlsList="noplaybackrate nodownload"
     disablePictureInPicture
-    oncontextmenu={handleContextMenu}
+    oncontextmenu={(e) => e.preventDefault()}
+    bind:volume
 ></video>

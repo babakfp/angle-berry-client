95de1f7d318eea4f6f24c8c4c758ccf23d8a440c
babakfp <babak.bxf@gmail.com>
2025-12-27 09:35:06 +0330

    remove unneeded variables

--- src/routes/(auth)/(lib)/schema.ts
+++ src/routes/(auth)/(lib)/schema.ts
@@ -1,33 +1,20 @@
 import { valibot } from "sveltekit-superforms/adapters"
 import * as v from "valibot"
 
-const USERNAME_PATTERN = /^[a-z]+$/
-const USERNAME_MIN_LENGTH = 4
-const USERNAME_MAX_LENGTH = 12
-const PASSWORD_MIN_LENGTH = 8
-const PASSWORD_MAX_LENGTH = 32
-
 export const schema = valibot(
     v.object({
         username: v.optional(
             v.pipe(
                 v.string(),
                 v.trim(),
-                v.minLength(USERNAME_MIN_LENGTH),
-                v.maxLength(USERNAME_MAX_LENGTH),
-                v.regex(
-                    USERNAME_PATTERN,
-                    "Only lowercase Latin letters are allowed",
-                ),
+                v.minLength(4),
+                v.maxLength(12),
+                v.regex(/^[a-z]+$/, "Only lowercase Latin letters are allowed"),
             ),
             "",
         ),
         password: v.optional(
-            v.pipe(
-                v.string(),
-                v.minLength(PASSWORD_MIN_LENGTH),
-                v.maxLength(PASSWORD_MAX_LENGTH),
-            ),
+            v.pipe(v.string(), v.minLength(8), v.maxLength(32)),
             "",
         ),
     }),

bc087be1ed4cad30e7b96399f0ea58ba469c8961
babakfp <babak.bxf@gmail.com>
2025-12-27 20:06:02 +0330

    update FormBase for remote functions

--- src/lib/components/form/FormBase.svelte
+++ src/lib/components/form/FormBase.svelte
@@ -1,95 +1,29 @@
 <script module lang="ts">
-    import type { ActionResult } from "@sveltejs/kit"
+    import type { RemoteForm, RemoteFormInput } from "@sveltejs/kit"
+    import type { Snippet } from "svelte"
+    import type { HTMLFormAttributes } from "svelte/elements"
 
     export type FormBaseProps<
-        Success extends Record<string, unknown> | undefined,
-        Failure extends Record<string, unknown> | undefined,
-    > = {
-        errors?: SuperForm<Record<string, unknown>>["errors"]
-        validateForm?: SuperForm<Record<string, unknown>>["validateForm"]
-        action?: string
-        allowUpload?: boolean
-        class?: ClassValue
-        children?: Snippet
-        onSubmit?: () => void
-        onInvalid?: () => void
-        onReturn?: () => void
-        onError?: () => void
-        onFailure?: (result: {
-            type: "failure"
-            status: number
-            data?: Failure
-        }) => void
-        onRedirect?: () => void
-        onSuccess?: () => void
-    }
+        Input extends RemoteFormInput | void,
+        Output,
+    > = RemoteForm<Input, Output>
+        & Omit<HTMLFormAttributes, "novalidate" | "enctype"> & {
+            allowUpload?: boolean
+            children?: Snippet // TODO: maybe doesn't need to be optional
+        }
 
-    //
+    // TODO
 </script>
 
-<script
-    lang="ts"
-    generics="Success extends Record<string, unknown> | undefined, Failure extends Record<string, unknown> | undefined"
->
-    import type { SubmitFunction } from "@sveltejs/kit"
-    import { type Snippet } from "svelte"
-    import type { ClassValue } from "svelte/elements"
-    import type { SuperForm } from "sveltekit-superforms/client"
-    import { enhance } from "$app/forms"
-
-    let {
-        errors,
-        validateForm,
-        action,
-        allowUpload,
-        class: class_,
-        children,
-        onSubmit,
-        onInvalid,
-        onReturn,
-        onError,
-        onFailure,
-        onRedirect,
-        onSuccess,
-    }: FormBaseProps<Success, Failure> = $props()
-
-    const handleFormSubmit: SubmitFunction = async ({ cancel }) => {
-        onSubmit?.()
-
-        if (validateForm && errors) {
-            const validation = await validateForm()
-
-            if (!validation.valid) {
-                onInvalid?.()
-                cancel()
-                errors.set(validation.errors)
-            }
-        }
-
-        return async ({ result, update }) => {
-            onReturn?.()
-            if (result.type === "error") {
-                onError?.()
-            } else if (result.type === "failure") {
-                // @ts-expect-error goh nakhor
-                onFailure?.(result)
-            } else if (result.type === "redirect") {
-                onRedirect?.()
-            } else if (result.type === "success") {
-                onSuccess?.()
-            }
-            update()
-        }
-    }
+<script lang="ts" generics="Input extends RemoteFormInput | void, Output">
+    let { allowUpload, children, ...rest }: FormBaseProps<Input, Output> =
+        $props()
 </script>
 
 <form
-    class={class_}
-    method="post"
-    use:enhance={handleFormSubmit}
     novalidate
-    {action}
     enctype={allowUpload ? "multipart/form-data" : undefined}
+    {...rest}
 >
     {@render children?.()}
 </form>

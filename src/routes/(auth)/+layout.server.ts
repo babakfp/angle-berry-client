09ac907e9380c4e26f769a333179ad6a8430d4ca
babakfp <babak.bxf@gmail.com>
2026-01-01 23:20:12 +0330

    remove +layout.server.ts

--- src/routes/(auth)/+layout.server.ts
+++
@@ -1,7 +0,0 @@
-import { redirect } from "@sveltejs/kit"
-import type { LayoutServerLoad } from "./$types"
-
-export const load: LayoutServerLoad = ({ locals }) => {
-    if (!locals.loggedInUser) return
-    redirect(303, "/")
-}

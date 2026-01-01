fa588a137fe79623e7ea686c0c3d3c94cf005413
babakfp <babak.bxf@gmail.com>
2026-01-01 22:58:42 +0330

    remove index file

--- src/lib/server/remotes/index.ts
+++
@@ -1,1 +0,0 @@
-export { allowNonLoggedInOnly } from "./allowNonLoggedInOnly.remote"
--- src/routes/(auth)/register/+page.svelte
+++ src/routes/(auth)/register/+page.svelte
@@ -4,7 +4,7 @@
     import Form from "$lib/components/form/Form.svelte"
     import PasswordField from "$lib/components/form/PasswordField.svelte"
     import UsernameField from "$lib/components/form/UsernameField.svelte"
-    import { allowNonLoggedInOnly } from "$lib/server/remotes"
+    import { allowNonLoggedInOnly } from "$lib/server/remotes/allowNonLoggedInOnly.remote"
     import {
         useIssue,
         useSnapshot,

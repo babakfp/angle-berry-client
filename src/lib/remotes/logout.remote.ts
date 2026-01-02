64e66a07af52681ef285e0649c09173d7ccd842c
babakfp <babak.bxf@gmail.com>
2026-01-02 17:50:59 +0330

    change logout form action

--- src/lib/remotes/logout.remote.ts
+++ src/lib/remotes/logout.remote.ts
@@ -4,11 +4,8 @@ import { form, getRequestEvent } from "$app/server"
 export const logout = form("unchecked", async () => {
     const { locals } = getRequestEvent()
 
-    if (!locals.loggedInUser) {
-        redirect(303, "/login")
-    }
-
     locals.pb.authStore.clear()
     locals.loggedInUser = undefined
+
     redirect(303, "/login")
 })

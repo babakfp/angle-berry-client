a10441c0c866a7dd30de49f8d3d955b3435b76b7
babakfp <babak.bxf@gmail.com>
2026-01-02 17:32:54 +0330

    add todo

--- src/lib/utilities/remote-forms/useSnapshot.ts
+++ src/lib/utilities/remote-forms/useSnapshot.ts
@@ -1,5 +1,7 @@
 import type { RemoteForm } from "@sveltejs/kit"
 
+// TODO: https://svelte.dev/docs/kit/remote-functions#form-Handling-sensitive-data
+// You can prevent sensitive data (such as passwords and credit card numbers) from being sent back to the user by using a name with a leading underscore:
 export const useSnapshot = (form: RemoteForm<any, unknown>) => {
     return {
         capture: () => form.fields.value(),
